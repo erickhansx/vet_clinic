2e0cb6358452aee6644aa840cd79c08c6d1995da
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2022-12-20 19:28:22 -0300

    add required data & queries comparison

--- data.sql
+++ data.sql
@@ -61,4 +61,11 @@ FROM owners
 WHERE owners.full_name='Dean Winchester' AND animals.name='Angemon' 
 OR owners.full_name='Dean Winchester' AND animals.name='Boarmon';
 
+INSERT INTO visits (id_animals, id_vet, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+
+
+
 
