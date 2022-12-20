0b4d4600078e3cd85fbe0167e9af461e61d825c3
erickhansx <erickhansx@gmail.com>
2022-12-20 16:18:46 -0600

    Add index on id animals and id vet, and email column

--- schema.sql
+++ schema.sql
@@ -34,3 +34,10 @@ ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
 ALTER TABLE animals ADD owner_id INTEGER;
 
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX visits_id_animals_id_vet_idx ON visits(id_animals,id_vet);
+
+CREATE INDEX owners_email_idx ON owners(email);
