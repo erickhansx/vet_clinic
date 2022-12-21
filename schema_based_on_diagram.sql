ccbf23612a7ab7d28016456585d48d7b860ec4c2
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2022-12-21 17:26:04 -0300

    create index by foreign key

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -47,3 +47,8 @@ ALTER TABLE
     "medical_histories" ADD CONSTRAINT "medical_histories_patient_id_foreign" FOREIGN KEY("patient_id") REFERENCES "patients"("id");
 ALTER TABLE
     "invoices" ADD CONSTRAINT "invoices_medical_history_id_foreign" FOREIGN KEY("medical_history_id") REFERENCES "medical_histories"("id");
+
+CREATE INDEX invoice_items_treatment_id_idx ON invoice_items (treatment_id);
+CREATE INDEX invoice_items_invoice_id_idx ON invoice_items (invoice_id);
+CREATE INDEX medical_histories_patient_id_idx ON medical_histories (patient_id);
+CREATE INDEX invoices_medical_history_id_idx ON invoices (medical_history_id);
